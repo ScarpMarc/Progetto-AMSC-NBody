64c7ee661d6e1900ff0d6e5d73e032d58c813e55
Marco De Rosso <marco.derosso@mail.polimi.it>
2022-12-01 11:37:51 +0100

    Implemented print function

--- Progetto AMSC NBody/Particle.cpp
+++ Progetto AMSC NBody/Particle.cpp
@@ -2,6 +2,29 @@
 #include "Particle.h"
 
 template<unsigned int dim>
+void Particle<dim>::print()
+{
+	constexpr char coord[3] = {'X', 'Y', 'Z'};
+	std::cout << "Particle ID: " << ID << std::endl;
+	std::cout << "Position";
+	for (int i = 0; i < dim; ++i)
+	{
+		std::cout << std::ios::right << std::setw(15) << coord[i] << ": " << pos[i] << std::endl;
+	}
+	std::cout << "Speed";
+	for (int i = 0; i < dim; ++i)
+	{
+		std::cout << std::ios::right << std::setw(15) << coord[i] << ": " << speed[i] << std::endl;
+	}
+	std::cout << "Acceleration";
+	for (int i = 0; i < dim; ++i)
+	{
+		std::cout << std::ios::right << std::setw(15) << coord[i] << ": " << speed[i] << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+template<unsigned int dim>
 Vector<dim> Particle<dim>::calcNewPosition(const unsigned int& delta_time)
 {
 	_updateSpeed(delta_time);
@@ -35,3 +58,12 @@ void Particle<dim>::_updatePos(const unsigned int& delta_time)
 		pos[i] = speed[i] * delta_time;
 	}
 }
+
+template<unsigned int dim>
+Vector<dim> Particle<dim>::calcForce(const Particle<dim>& other) const
+{
+	Vector<dim> dispalcement = this->calcDistance(other);
+	double distance = dispalcement.eu_norm();
+	return -
+}
+ 
