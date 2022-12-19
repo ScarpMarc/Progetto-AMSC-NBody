eebe4244836ebcab323393a67a6a43a83892ced8
Overlord2410 <mtrigam@gmail.com>
2022-12-19 16:06:11 +0000

    added json parser

--- Progetto AMSC NBody/Constants.h
+++ Progetto AMSC NBody/Constants.h
@@ -16,13 +16,13 @@ const unsigned int DIM = 3;
 /// <summary>
 /// Amount of ticks computed during the simulation
 /// </summary>
-const unsigned long int max_ticks = 10000;
+unsigned long int max_ticks = 10000;
 
 /// <summary>
 /// Amount of tick per simulation second.
 /// Note: this has no connection to real-time seconds, but is only simulation-related.
 /// </summary>
-const double ticks_per_second = 10;
+double ticks_per_second = 10;
 
 //
 // GRAPHICS
@@ -31,13 +31,13 @@ const double ticks_per_second = 10;
 /// <summary>
 /// Amount of real-time milliseconds that the drawing routine pauses for after each drawing cycle.
 /// </summary>
-const unsigned int screen_refresh_millis = 100;
+unsigned int screen_refresh_millis = 100;
 
 /// <summary>
 /// Screen resolution in the horizontal direction
 /// </summary>
-const unsigned int screenResX = 1024;
+unsigned int screenResX = 1024;
 /// <summary>
 /// Screen resolution in the vertical direction
 /// </summary>
-const unsigned int screenResY = 1024;
+unsigned int screenResY = 1024;
