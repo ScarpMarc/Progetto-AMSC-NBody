b70e6051a63e153a65c38b45027a58c054ce54f3
Marco <underscore.marczero@gmail.com>
2022-12-28 00:15:07 +0000

    Moved an omp directive and sped up the programme

--- Progetto AMSC NBody/simulation_functions.h
+++ Progetto AMSC NBody/simulation_functions.h
@@ -4,23 +4,27 @@
 
 #include "Particle.h"
 
-template<unsigned int dim>
-int do_simulation_step(std::vector<Particle<dim>>& particles, const unsigned int& ticks_step)
+template <unsigned int dim>
+int do_simulation_step(std::vector<Particle<dim>> &particles, const unsigned int &ticks_step)
 {
-//#pragma omp parallel for
+#pragma omp parallel for
 	for (int i = 0; i < particles.size(); ++i)
 	{
 		Vector<dim> temp;
-		for (unsigned int component = 0; component < dim; ++component)
+		/*for (unsigned int component = 0; component < dim; ++component)
 		{
 			double component_force_on_particle = 0; // Automatically initialised to 0
-#pragma omp parallel for reduction(+: component_force_on_particle)
+//#pragma omp parallel for reduction(+: component_force_on_particle)
 			for (int j = 1; j < particles.size(); ++j)
 			{
 				component_force_on_particle += particles[i].calcForce(particles[(i + j) % particles.size()])[component];
 			}
 
 			temp[component] = component_force_on_particle;
+		}*/
+		for (int j = 1; j < particles.size(); ++j)
+		{
+			temp += particles[i].calcForce(particles[(i + j) % particles.size()]);
 		}
 
 		particles[i].updateResultingForce(temp);
